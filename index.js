b64ea5fbd051a2051d9be2b5106e79f2207123a3
Rkafluk29 <77253025+fluk55567@users.noreply.github.com>
2023-11-02 09:06:33 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function feature1() {
+    alert('from feature 1.');
+}
+
+function feature2() {
+    //comment from B
+    alert('from feature 2.');
+}
+
+function feature3() {
+    alert('from feature 3.')
+}
